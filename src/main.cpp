05d3a99059b962053e59ddb017015a372a62c701
Tomasz Scislowicz <toomasz@gmail.com>
2022-07-30 14:03:43 +0200

    Shelly I3 implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,78 @@
 #include <Arduino.h>
 #include <ThingifyEsp.h>
+#include <Lib\DebounceButton\DebounceButton.h>
 
-ThingifyEsp thing("Sample Thing");
+ThingifyEsp thing("Shelly I3");
 
-bool OnBoolChanged(void*_, Node *node)
-{
-    bool value = node->Value.AsBool();
-    if(value)
-    {
-        Serial.printf("Bool node changed to true");
-    }
-    else
-    {
-        Serial.printf("Bool node changed to false");
-    }
-    return true;
-}
 
-bool OnNodeValueChanged(void*_, Node *node)
-{
-    Serial.printf("'%s' node changed value to '%s'\n", node->name(), node->Value.toString().c_str());
-    return true;
-}
+// pin definitions
+const int Switch1Pin = 14;
+const int Switch2Pin = 12;
+const int Switch3Pin = 13;
 
-NodeValue FunctionExecuted(void* context, FunctionArguments &args)
-{
-    return NodeValue::String("Hello from function!");
-}
+const int ResetButtonPin = 2;
 
+// nodes
+Node *switch1Node, *switch2Node, *switch3Node;
+
+//buttons
+DebounceButton switch1(Switch1Pin, INPUT);
+DebounceButton switch2(Switch2Pin, INPUT);
+DebounceButton switch3(Switch3Pin, INPUT);
+
+// variables
+bool _wasResetPressed = false;
+uint64_t _resetPressStartTime = 0;
+uint64_t _lastTemperatureRead = 0;
 
 void setup()
 {
     Serial.begin(500000);
-
+    thing.Initialize();
     thing.AddDiagnostics();
 
-    thing.AddBoolean("bool")->OnChanged(OnNodeValueChanged);
-    thing.AddFunction("Function", FunctionExecuted, nullptr);
-    thing.AddInt("integer", ThingifyUnit::Volt)->SetValue(NodeValue::Int(220));
-    thing.AddRange("range", 10, 30, 2, ThingifyUnit::Percent)->OnChanged(OnNodeValueChanged);
-    thing.AddColor("Color")->OnChanged(OnNodeValueChanged);
-    
+    switch1.Init();
+    switch2.Init();
+    switch3.Init();
+
+    pinMode(ResetButtonPin, INPUT);
+
+    switch1Node = thing.AddBoolean("Switch1");
+    switch2Node = thing.AddBoolean("Switch2");
+    switch3Node = thing.AddBoolean("Switch3");
+
     thing.Start();
 }
 
-
-
 void loop()
 {
+    switch1.Loop();
+    switch2.Loop();
+    switch3.Loop();
+    
+    switch1Node->Set(switch1.IsPressed());
+    switch2Node->Set(switch2.IsPressed());
+    switch3Node->Set(switch3.IsPressed());
+
+   // detect button reset long press
+    bool isResetPressed = !digitalRead(ResetButtonPin);
+
+    if(!_wasResetPressed && isResetPressed)
+    {
+        _resetPressStartTime = millis();
+    }
+
+    if(!isResetPressed)
+    {
+        _resetPressStartTime = 0;
+    }    
+
+    if(_resetPressStartTime != 0 && millis() - _resetPressStartTime > 5000)
+    {
+        thing.ResetConfiguration();
+    }
+    _wasResetPressed = isResetPressed;
+    // end detect button reset long press
+
     thing.Loop();
 }
